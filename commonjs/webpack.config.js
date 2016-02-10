f33b532d627d27ad6c222a635f49a518a251bdef
Nicholas Beach <nicholascbeach@gmail.com>
2016-02-10 09:13:01 -0500

    Redordered webpack config

--- commonjs/webpack.config.js
+++ commonjs/webpack.config.js
@@ -1,11 +1,11 @@
 var webpack = require('webpack');
 
 var webpackConfig = {
+  entry: './src/commonjs.js',
   output: {
     path: "./compliled",
     filename:'bundle.js'
-  },
-  entry: './src/commonjs.js'
+  }
 };
 
 module.exports = webpackConfig;
